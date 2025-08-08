121abd993bd7f73cab3155cb059d947bf2c1ba17
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2025-08-08 01:23:59 +0100

    chore: make lazyvim terminal pretty

---
+++ .config/nvim/lua/config/terminal.lua
@@ -0,0 +1,9 @@
+-- Terminal configuration
+vim.opt.shell = "/bin/zsh"
+
+-- Set shell options for better integration
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+
+return {}
